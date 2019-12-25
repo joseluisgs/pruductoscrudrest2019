319fc4ada456c7f886084a51699c0de5d2216123
joseluisgs <joseluisgonsan@gmail.com>
2019-12-25 13:40:52 +0100

    Initial commit

---
+++ src/main/java/com/example/pruductoscrudrest/PruductoscrudrestApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruductoscrudrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruductoscrudrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruductoscrudrestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pruductoscrudrest/PruductoscrudrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruductoscrudrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruductoscrudrestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
