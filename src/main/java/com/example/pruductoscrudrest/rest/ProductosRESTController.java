7ee1bdcf359a30c717fd34e1a4dc890ec9341ce9
joseluisgs <joseluisgonsan@gmail.com>
2019-12-25 14:04:34 +0100

    Método hola usando GetMapping y/o RequestMapping

--- src/main/java/com/example/pruductoscrudrest/rest/ProductosRESTController.java
+++ src/main/java/com/example/pruductoscrudrest/rest/ProductosRESTController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/")    // Definimos la url o entrada de la API REST
 public class ProductosRESTController {
 
-    // Exponemos cada servicio
+    /**
     //@RequestMapping @GetMapping @PostMapping (estos dos últimos no deginen la URL, si no toman la url base
     //@GetMapping Consume el servicio en la url raiz usando GET
     // @RequestMapping(value = "hola", method = RequestMethod.GET) de esta manera indicamos el protocolo y la URL de entrada
+    */
 
     //@GetMapping
     //@RequestMapping(value = "hola", method = RequestMethod.GET)
